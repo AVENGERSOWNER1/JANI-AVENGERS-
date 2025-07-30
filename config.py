e5663210fa4bde143bea3ccc4f0ec2f899b59e80
JaniPrivetRepo <fffdfhhbnjjh@gmail.com>
2025-07-30 21:56:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ramrk399/JANI-RP-LOVRR-413",
+    "https://github.com/JaniPrivetRepo/JANI-RP-LOVRR",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
